dfd38d74172260b06dd18fa1ab4f0dd8d72ca485
CyJackX <42789314+CyJackX@users.noreply.github.com>
2025-12-11 00:07:48 -0500

    Video Lists

---
+++ src/data/videos.ts
@@ -0,0 +1,295 @@
+import type { VideoGroup } from "../utils/videoEmbeds";
+
+export const videoGroups: VideoGroup[] = [
+  {
+    label: "Tootsie's 4",
+    date: "2025-06-06",
+    videos: [
+      {
+        title: "Gaytheon",
+        url: "https://www.youtube.com/watch?v=yHCHWnA81xc",
+        description: { credits: "Bill DiPiero" },
+      },
+      {
+        title: "1-666-Murdered",
+        url: "https://youtu.be/NLq-vTWDw0E",
+        description: { credits: "Gregory Gallagher" },
+      },
+      {
+        title: "Online Dating",
+        url: "https://youtu.be/Zs6tllyIzWE?si=kI1v5WBD59Gs5XYk",
+        description: { credits: "Carla Jerez" },
+      },
+
+      {
+        title: "Heist",
+        url: "https://www.youtube.com/watch?v=yAvmZeCKdHg",
+        description: { credits: "Rajiv Rao" },
+      },
+      {
+        title: "The Tree & Me",
+        url: "https://youtu.be/tHfLHRO2j8E?si=5CNSoA3tDqVm_gap",
+        description: { credits: "Eric Feurer" },
+      },
+      //   {
+      //     title: "Farewell to a Friend",
+      //     url: "https://vimeo.com/1104256244?share=copy#t=0",
+      //     description: { credits: "Hannah Kaplan" },
+      //   },
+      {
+        title: "The Only Morning Show",
+        url: "https://www.youtube.com/watch?v=I4aQrFztoQ8",
+        description: { credits: "Joe Miles" },
+      },
+      {
+        title: "Bushwick Bateman",
+        url: "https://youtu.be/HUoT35eksMY?si=ayeBAL9_ONWiJcVX",
+        description: { credits: "Ben Strate" },
+      },
+      //   {
+      //     title: "We Got Cursed by a Bog Witch",
+      //     url: "https://www.instagram.com/p/DK2snNFR1v_/",
+      //     description: { credits: "Miller Bradford" },
+      //   },
+
+      //   {
+      //     title: "Ifs",
+      //     url: "https://www.instagram.com/reel/DKuVCDFxXTe/",
+      //     description: { credits: "Hoff Matthews" },
+      //   },
+      //   {
+      //     title: "Aftercare",
+      //     url: "https://vimeo.com/1097162980?share=copy",
+      //     description: { credits: "Andrew Bourne" },
+      //   },
+      {
+        title: "The Michelles",
+        url: "https://youtu.be/WeepjF5v2Qk",
+        description: { credits: "Katherine Coleman" },
+      },
+      //   {
+      //     title: "Time To Go Back",
+      //     url: "https://www.youtube.com/watch?v=pBFoezAa-cQ",
+      //     description: { credits: "Brianna Lee" },
+      //   },
+      {
+        title: "Vagalis",
+        url: "https://youtu.be/xnQDK29XCqc",
+        description: { credits: "Anita Castillo-Halvorssen" },
+      },
+      {
+        title: "Pregnancy Test",
+        url: "https://www.youtube.com/watch?v=6qLeT2MyLwA",
+        description: {
+          credits: "Jared Bronen",
+          details: "Best Actor - Marina Gasparyan",
+        },
+      },
+      //   {
+      //     title: "Game Night",
+      //     url: "https://www.instagram.com/p/DMQOsjWubh1/",
+      //     description: { credits: "Drew Luster" },
+      //   },
+    ],
+  },
+  {
+    label: "Tootsie's 3",
+    date: "2024-05-31",
+    videos: [
+      {
+        title: "When Jacob Jinkers Came to Town (Trailer)",
+        url: "https://vimeo.com/1116278508",
+        description: { credits: "Max Levine and Alex Schulman" },
+      },
+      {
+        title: "Unfamiliar",
+        url: "https://vimeo.com/832076218?fl=pl&fe=vl",
+        description: { credits: "Sophia Marshall" },
+      },
+      {
+        title: "To Kill a Spider",
+        url: "https://vimeo.com/950567263/1432e8af23",
+        description: {
+          credits: "Joe Natalzia, Alexa Kresojevich, and Shawn Gray",
+        },
+      },
+      {
+        title: "For the Love of the Game",
+        url: "https://www.youtube.com/watch?v=DpUbOmEOUNc",
+        description: { credits: "Bill DiPiero, Alexis Pereira, Paris Adkins" },
+      },
+      {
+        title: "Staying In",
+        url: "https://www.youtube.com/watch?v=ahAP5rpuXvI",
+        description: { credits: "Drew Luster" },
+      },
+      {
+        title: "Mark & Lisa",
+        url: "https://www.youtube.com/watch?v=D-zVdbrBwqo",
+        description: { credits: "Marina Gasparyan and Maarit Hara" },
+      },
+      {
+        title: "Chemistry Test",
+        url: "https://www.youtube.com/watch?v=frkhmBFoJlE",
+        description: { credits: "Tim Barnes, Andrew Sanford, and Jim Fagan" },
+      },
+      {
+        title: "Food Fight",
+        url: "https://www.youtube.com/watch?v=gtyLIX8Frgc",
+        description: { credits: "Off Q Comedy" },
+      },
+      {
+        title: "Did We...?",
+        url: "https://www.youtube.com/watch?v=cKr58JJ6bgo",
+        description: { credits: "Colin O'Brien" },
+      },
+      {
+        title: "Googley Eyes",
+        url: "https://www.youtube.com/watch?v=hGIfBHPkqBc",
+        description: {
+          credits:
+            "Trevor Williams, starring Masha King, Vincent Peone, Katie Sicking, Andrew Bourne. Shot by Nathan Russell.",
+        },
+      },
+      {
+        title: "SCRAPBOOKING",
+        url: "https://www.youtube.com/watch?v=mM5wVPIytiM",
+        description: {
+          credits: "SCRAPBOOKING by Will Jacobs and Matt Antonucci",
+        },
+      },
+      {
+        title: "Pick a Random Number",
+        url: "https://www.youtube.com/watch?v=Plx-cJik2n8&t=1s",
+        description: { credits: "Jared Bronen" },
+      },
+      {
+        title: "Muntnertown",
+        url: "https://www.youtube.com/watch?v=PQ_ZPaQW3Vs",
+        description: { credits: "Mike Muntner" },
+      },
+      {
+        title: "Cool Momz",
+        url: "https://www.youtube.com/watch?v=tQ9IBt_RE_g",
+        description: { credits: "Andrew Bourne" },
+      },
+    ],
+  },
+  {
+    label: "Tootsie's 2",
+    date: "2023-10-13",
+    videos: [
+      {
+        title: "Killer Audition",
+        url: "https://www.youtube.com/watch?v=r1zIXy5uOXM",
+        description: { credits: "Dinner for One and Steven Bailey" },
+      },
+      {
+        title: "1998",
+        url: "https://www.youtube.com/watch?v=TbND8oeOG7Q",
+        description: { credits: "Alex Estrada and Drew Luster" },
+      },
+      {
+        title: "JK Bullworth",
+        url: "https://vimeo.com/710506507?fl=pl&fe=vl",
+        description: { credits: "Sam O'Sullivan" },
+      },
+      {
+        title: "Melinda's Late Night Radio Talk Show",
+        url: "https://www.youtube.com/watch?v=3F4JrzmI2vU",
+        description: { credits: "Laura Ornella & Kevin Ralston" },
+      },{
+        title: "The Partner",
+        url: "https://www.youtube.com/watch?v=pnW4EZqCRKk",
+        description: { credits: "Paris Adkins, Alexis Pereira, Bill DiPiero" },
+      },
+      {
+        title: "Oh! Their Story Is Better Than Mine?!",
+        url: "https://www.youtube.com/watch?v=cXo0GWx8Rq0",
+        description: { credits: "Colin O'Brien & Drew Luster" },
+      },
+      {
+        title: "Self Love (Official Music Video)",
+        url: "https://www.youtube.com/watch?v=SMK5YMOP0Gw",
+        description: { credits: "Josh Nasser" },
+      },
+      {
+        title: "Her Pleasure",
+        url: "https://www.youtube.com/watch?v=imyC_eJ444Y",
+        description: { credits: "Sludge Rudder" },
+      },
+      {
+        title: "Time Traveler Party",
+        url: "https://www.youtube.com/watch?v=b8uO8kjoLIc",
+        description: { credits: "Hoff Matthews" },
+      },
+      {
+        title: "Old Friends",
+        url: "https://www.youtube.com/watch?app=desktop&v=STogahT-PzQ",
+        description: { credits: "Writer, Star, Producer" },
+      },
+      {
+        title: "Miniature Gentlemen",
+        url: "https://www.youtube.com/watch?v=gZ8cvB7Z6_I",
+        description: {
+          credits: "Laura Merli: Writer/VO Actor, Joe Bell: animator",
+        },
+      },
+    ],
+  },
+  {
+    label: "Tootsie's 1",
+    date: "2023-06-30",
+    videos: [
+      {
+        title: "French Lesbian Nun Drama",
+        url: "https://www.youtube.com/watch?v=hl9O_2XzrPE",
+        description: { credits: "Mike Muntner" },
+      },
+      {
+        title: "Pest Control",
+        url: "https://www.youtube.com/watch?v=13bx9an6py0",
+        description: { credits: "Tyler DeMaise" },
+      },
+
+      {
+        title: "The Clown",
+        url: "https://vimeo.com/345073874?fl=pl&fe=cm",
+        description: { credits: "Tessa Greenberg" },
+      },
+      {
+        title: "National Horn Association",
+        url: "https://www.youtube.com/watch?v=EEhOEQ1iMkw",
+        description: { credits: "Trevor Williams" },
+      },
+      {
+        title: "Variations on a Theme",
+        url: "https://vimeo.com/950880934",
+        description: { credits: "Peter Campbell" },
+      },
+
+      {
+        title: "Tell My Wife",
+        url: "https://www.youtube.com/watch?v=daNydp3mpI0",
+        description: { credits: "Katie & Andy" },
+      },
+      {
+        title: "A Great Day",
+        url: "https://www.youtube.com/watch?v=UFbvkrDWB-s",
+        description: { credits: "Alec Cohen & Connor Bowen" },
+      },
+      {
+        title: "Grindset",
+        url: "https://www.youtube.com/watch?v=Vm1TFYEf0NI",
+        description: { credits: "Bill DiPiero & Paris Adkins" },
+      },
+      {
+        title: "The Champ from Bleecker Street",
+        url: "https://www.youtube.com/watch?v=zNVdxD4vt2k",
+        description: {
+          credits: "Lauren Greenhall, David Bluvband",
+        },
+      },
+    ],
+  },
+];
