a3608c8d06f4a4606748fd35e50c37a9e3789115
CyJackX <42789314+CyJackX@users.noreply.github.com>
2026-03-28 22:54:27 -0400

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
+import { SITE_URL } from "./src/consts";
 
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
+	site: SITE_URL,
+	integrations: [sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
+export const SITE_NAME = "Tootsie's Video Vault";
+export const SITE_URL = "https://tootsiesvideovault.com";
 export const SITE_TITLE = "Tootsie's Video Vault";
-export const SITE_DESCRIPTION = "Tootsie’s Video Vault is an annual short film race organized by seasoned comedy film producers, featuring dozens of shorts created by local filmmakers!";
+export const SITE_DESCRIPTION =
+  "Tootsie's Video Vault is an annual short film race organized by seasoned comedy film producers, featuring dozens of shorts created by local filmmakers!";
+export const SITE_EMAIL = "tootsiesvideovault@gmail.com";
+export const SITE_INSTAGRAM = "https://www.instagram.com/tootsiesvideovault/";
