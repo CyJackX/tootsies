afde67b72bc1cdbb27b20beb4b820fc40a26d1b4
CyJackX <42789314+CyJackX@users.noreply.github.com>
2026-03-29 00:09:02 -0400

    Title improvement

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const SITE_NAME = "Tootsie's Video Vault";
 export const SITE_URL = "https://tootsiesvideovault.com";
-export const SITE_TITLE = "Tootsie's Video Vault";
+export const SITE_TITLE = "Tootsie's Video Vault - Bi-Annual NYC Short Film Jam";
 export const SITE_DESCRIPTION =
   "Tootsie's Video Vault is an annual short film race organized by seasoned comedy film producers, featuring dozens of shorts created by local filmmakers!";
 export const SITE_EMAIL = "tootsiesvideovault@gmail.com";
